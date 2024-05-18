7d6ba50c4dd837ade8c0e10c669ff15e48a4a719
Julio Bacoli <julio-microlins@hotmail.com>
2024-05-18 17:52:37 -0300

    codificando o cornometro

--- ProjetoCronometro/Program.cs
+++ ProjetoCronometro/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿StartCronometro();
+
+void StartCronometro()
+{
+    Console.WriteLine("Cronometro Iniciado");
+    int tempo = 10;
+    int currentTime = 0;
+
+    while (true)
+    {
+        Console.Clear();
+        Console.WriteLine(currentTime);
+        Thread.Sleep(1000);
+
+        currentTime++;
+
+        if (currentTime == tempo)
+        {
+            Console.Clear();
+            Console.WriteLine(currentTime);
+            break;
+        }
+    }
+
+}
